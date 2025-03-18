64235820f13399f81fd53ec7e60c6614ae31aaed
柏廷 陳 <poting1005@outlook.com>
2025-03-18 13:46:48 +0800

    新增各站enum

---
+++ DCU_Frame/DU_ID.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCU_Frame
+{
+    /// <summary>
+    /// 當站顯示器的ID 上下月台的顯示器使用數字17-24作為上行到下行的月台ID
+    /// </summary>
+    public  enum DU_ID
+    {
+        LG01_CDU_01,
+        LG01_CDU_02,
+        LG01_CDU_03,
+        LG01_CDU_04,
+        LG01_SDU_05,
+        LG01_SDU_06,
+        LG01_SDU_07,
+        LG01_PDU_17,
+        LG01_PDU_18,
+        LG01_PDU_19,
+        LG01_PDU_20,
+        LG01_PDU_21,
+        LG01_PDU_22,
+        LG01_PDU_23,
+        LG01_PDU_14
+    }
+}
--- TaskCDU/ProcTaskCDU.cs
+++ TaskCDU/ProcTaskCDU.cs
@@ -11,7 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Xml.Linq;
-using Newtonsoft.Json;
+
 
 
 
